a8f2c1da85bab16edeb5b104537f3b232dcf61a9
isaiahsilvani <isaiahsilvani@outlook.com>
2021-03-25 23:47:27 -0400

    css background

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,12 +2,13 @@ const Job = require('../models/job')
 
 function index(req, res) {
     console.log('index router hit')
-    res.render('jobs/index', { title: 'Find Jobs', user: req.user, results: null})
+    res.render('jobs/index', { title: 'Find Jobs', user: req.user, results: [""]})
 }
 
 function search(req, res) {
     console.log('search controller function hit')
-    res.redirect('jobs/index')
+    console.log(req.body)
+    res.redirect('/jobs/')
 }
 
 module.exports = {
