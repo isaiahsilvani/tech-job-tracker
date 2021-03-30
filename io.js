fdc5af9aaa7536d0bd75fdc4975901794e744fbe
isaiahsilvani <isaiahsilvani@outlook.com>
2021-03-30 15:15:13 -0400

    deploy to Heroku

--- io.js
+++ io.js
@@ -1,21 +1,10 @@
 const io = require('socket.io')()
-
+console.log(io)
 let hunters = {}
 
-
 io.on('connection', (socket) => {
-    
-    // When someone enters the room, add them to chatroom
-    // this socket is listenening for a register user event from any client
-    socket.on('register-user', () => {
-        // when user event 'register-user' happens, broadcast (emit) a response
-        io.emit('user-enter')
-    })
-     
-    // when someone leaves the room
-    socket.on('disconnect', () => {
-        io.emit('user-exit')
-    })
+
 })
+
 
 module.exports = io
