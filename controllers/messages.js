eac943ac7be0c66c0c358ced009509a980b2170e
isaiahsilvani <isaiahsilvani@outlook.com>
2021-03-31 11:13:42 -0400

    basic message functionality

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,16 +1,25 @@
 const Message = require('../models/message')
 
 module.exports = {
-    index
+    index,
+    create
 }
 
-function index(req, res) {
-    Message.find({})
-      .then((messages) => {
-      res.render("messages/index", {
-        title: "Message Board",
-        user: req.user,
-        messages: messages.reverse(),
-      });
+function create(req, res) {
+    req.body.postedBy = req.user.name;
+    req.body.avatar = req.user.avatar;
+    Message.create(req.body)
+      .then(() => {
+      res.redirect("/messages");
     });
   }
+
+function index(req, res) {
+    Message.find({}, function(err, messages) {
+        res.render('messages', {
+            title: 'Message Board',
+            user: req.user,
+            messages
+            })
+    })
+}
