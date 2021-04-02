5ecead0fcfed446b99f6c2033453b075baf5edaf
isaiahsilvani <isaiahsilvani@outlook.com>
2021-04-02 11:33:57 -0400

    polish

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const logger = require("morgan");
 const session = require("express-session");
 const passport = require("passport");
 const methodOverride = require("method-override");
-const htmlToText = require('html-to-text')
 
 // load env variables
 require("dotenv").config();
